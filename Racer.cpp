898b87c8e612b5b20c1230e454f737d30dcbf30f
James Haines <jdhaines@tesla.cs.iupui.edu>
2017-02-13 12:22:33 -0500

    figured out advance racer

--- Racer.cpp
+++ Racer.cpp
@@ -4,10 +4,38 @@
 
 #define RACE_LENGTH 50
 
-/*void advanceRacerA(int* ptrRacerA);
-void advanceRacerB(int* ptrRacerB);
-void printPosition(int* ptrRacerA, int* ptrRacerB);
-*/
+void advanceRacerA(int* ptrRacerA);
+/*void advanceRacerB(int* ptrRacerB);
+void printPosition(int* ptrRacerA, int* ptrRacerB);*/
+
+void advanceRacerA (int* ptrRacerA)
+{
+	//random number generator
+	srand (time(NULL));
+	
+	int obstacleNumA;
+	
+	//pick number between one and 100
+	obstacleNumA = rand() % 100 + 1;
+	
+	//determine obstacle by rand number and percentages
+	if (obstacleNumA <= 30){
+		*ptrRacerA = (*ptrRacerA) + 4;
+	}else if(31 <= obstacleNumA && obstacleNumA <= 40) {
+		*ptrRacerA = (*ptrRacerA) + 5;
+	}else if(41 <= obstacleNumA && obstacleNumA <= 70) {
+		*ptrRacerA = (*ptrRacerA) - 2;
+	}else if(71 <= obstacleNumA && obstacleNumA <= 90) {
+		*ptrRacerA = (*ptrRacerA) - 3;
+	}else if(91 <= obstacleNumA && obstacleNumA <= 100) {
+		*ptrRacerA = (*ptrRacerA) - 4;
+	}
+	
+	return;
+}
+	
+	
+	
 int main()
 {
 	//set ptr's to zero for starting position
@@ -15,15 +43,13 @@ int main()
 	int* ptrRacerA;
 	int* ptrRacerB;
 	
-	int racerA = 0, racerB = 0;
+	int racerA, racerB;
 	
 	ptrRacerA = &racerA;
 	ptrRacerB = &racerB;
 	
-	
-	//set obstacle name to char array in order to compare later
-	std::string obstacleA = "";
-	std::string obstacleB = "";
+	*ptrRacerA = 0;
+	*ptrRacerB = 0;
 	
 	//char array for race in order to print later
 	std::string race(RACE_LENGTH, ' ');
@@ -33,25 +59,17 @@ int main()
 	//while loop to repeat until someone wins
 	while (keepGoing == 0) {
 		
-		//test fill array with white space plus end line
-		race[5] = 'A';
-		std::cout << race << std::endl;
+		advanceRacerA (ptrRacerA);
+		
+		/*//test fill array with white space plus end line
+		race[ptrRacerA] = 'A';
+		
+		std::cout << race + '|' << std::endl;*/
+		std::cout << *ptrRacerA << std::endl;
 		keepGoing = 1;
 	
 
 	}
 	
-	
-	
-	
-	/* random number generator
-	srand (time(NULL));
-	
-	obstacleNumA = rand() % 100 + 1;
-	obstacleNumB = rand() % 100 + 1;
-	
-	std::cout << obstacleNumA << std::endl;
-	std::cout << obstacleNumB << std::endl;
-	*/
 	return 0;
 }
